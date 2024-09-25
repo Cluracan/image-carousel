cc4ac00efd682c3c6de7f9dc3a37b67be3a3e052
cluracan1 <cluracan1@gmail.com>
2024-09-25 12:11:34 +0100

    Initial  commit - slideplayer implemented

--- main.js
+++ main.js
@@ -1,24 +1,55 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import "./style.css";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+//insert images into image-holder
+const imageHolder = document.getElementById("image-holder");
+const images = import.meta.glob("./images/*");
+for (const imageUrl in images) {
+  const image = document.createElement("img");
+  image.classList.add("image");
+  image.src = imageUrl;
+  imageHolder.appendChild(image);
+}
+//add an additional copy of first image to end
+const firstImageCopy = document.createElement("img");
+firstImageCopy.classList.add("image");
+console.log(Object.keys(images)[0]);
+firstImageCopy.src = Object.keys(images)[0];
+imageHolder.appendChild(firstImageCopy);
 
-setupCounter(document.querySelector('#counter'))
+//variables
+let currentIndex = 0;
+const numberOfImages = Object.keys(images).length + 1;
+
+//slideplayer
+const intervalId = setInterval(autoTranslate, 2000);
+
+//left/right arrows
+
+//functions
+function autoTranslate() {
+  if (currentIndex === numberOfImages - 1) {
+    currentIndex = 0;
+  }
+  const startIndex = currentIndex;
+  currentIndex = (currentIndex + 1) % numberOfImages;
+  console.log(`now ${currentIndex}`);
+  translateImageHolder(startIndex, currentIndex);
+}
+
+function translateImageHolder(startIndex, endIndex) {
+  const testTransform = [
+    {
+      transform: `translateX(calc(var(--frame-width) * -${startIndex}))`,
+    },
+    {
+      transform: `translateX(calc(var(--frame-width) * -${endIndex}))`,
+    },
+  ];
+
+  const testTransformTiming = {
+    duration: 1000,
+    fill: "forwards",
+  };
+
+  imageHolder.animate(testTransform, testTransformTiming);
+}
