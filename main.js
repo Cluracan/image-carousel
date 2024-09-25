70d4508d6cfa11d5aee7594bf03fe7e8dfe5990a
cluracan1 <cluracan1@gmail.com>
2024-09-25 13:16:18 +0100

    insert left/right arrows, 'pause' feature added to left arrow. left arrow click implemented

--- main.js
+++ main.js
@@ -12,28 +12,44 @@ for (const imageUrl in images) {
 //add an additional copy of first image to end
 const firstImageCopy = document.createElement("img");
 firstImageCopy.classList.add("image");
-console.log(Object.keys(images)[0]);
 firstImageCopy.src = Object.keys(images)[0];
 imageHolder.appendChild(firstImageCopy);
 
 //variables
 let currentIndex = 0;
+let pauseTransition = false;
 const numberOfImages = Object.keys(images).length + 1;
 
 //slideplayer
 const intervalId = setInterval(autoTranslate, 2000);
 
 //left/right arrows
+const leftArrow = document.getElementById("left-arrow");
+leftArrow.addEventListener("click", (e) => {
+  if (currentIndex === 0) {
+    currentIndex = numberOfImages - 1;
+  }
+  const startIndex = currentIndex;
+  currentIndex = (currentIndex - 1) % numberOfImages;
+  translateImageHolder(startIndex, currentIndex);
+});
+leftArrow.addEventListener("mouseover", (e) => {
+  pauseTransition = true;
+});
+leftArrow.addEventListener("mouseout", (e) => {
+  pauseTransition = false;
+});
 
 //functions
 function autoTranslate() {
-  if (currentIndex === numberOfImages - 1) {
-    currentIndex = 0;
+  if (!pauseTransition) {
+    if (currentIndex === numberOfImages - 1) {
+      currentIndex = 0;
+    }
+    const startIndex = currentIndex;
+    currentIndex = (currentIndex + 1) % numberOfImages;
+    translateImageHolder(startIndex, currentIndex);
   }
-  const startIndex = currentIndex;
-  currentIndex = (currentIndex + 1) % numberOfImages;
-  console.log(`now ${currentIndex}`);
-  translateImageHolder(startIndex, currentIndex);
 }
 
 function translateImageHolder(startIndex, endIndex) {
